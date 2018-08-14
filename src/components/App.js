4f8d7ef7929ec8b9a7653eb795eef9974b453a09
tspeed90 <tspeed90@gmail.com>
2018-08-14 15:28:15 +0100

    add App.js and render App

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Soon to be stashpoints!</div>;
+  }
+}
