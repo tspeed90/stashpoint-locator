7310d8180b96aec7535c384d560c9930c65cb855
tspeed90 <tspeed90@gmail.com>
2018-08-14 16:00:41 +0100

    add util fetch function to query API for stashpoints

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-
+import { getStashpoints } from '../utils/getStashpoints';
 export default class App extends Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    getStashpoints('London');
   }
 
   render() {
---
+++ src/utils/getStashpoints.js
@@ -0,0 +1,14 @@
+const checkResponse = response => {
+  if (response.status !== 200) {
+    console.log('There was an error with the request');
+    return;
+  }
+  return response.json();
+};
+
+export const getStashpoints = city => {
+  const stasherURL = 'https://api-staging.stasher.com/v1/stashpoints';
+  return fetch(`${stasherURL}?city=${city}`)
+    .then(checkResponse)
+    .then(res => console.log(res));
+};
